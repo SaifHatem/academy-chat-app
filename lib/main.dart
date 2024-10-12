539f7abb201c89d8027853c24219d7e75d80481a
Saif Hatem <shatem793@gmail.com>
2024-10-12 16:51:35 +0300

    finsih splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:academy_chat_app/features/splash/presentation/views/splash_view.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
-    const MyApp(),
-    // DevicePreview(
-    //   enabled: !kReleaseMode,
-    //   builder: (context) => const MyApp(), // Wrap your app
-    // ),
+    //const MyApp(),
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
   );
 }
 
